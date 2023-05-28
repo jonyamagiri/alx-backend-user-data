0ebe920af66cbd397180dd7534f8cc198d4d5eee
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-28 15:23:46 +0300

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ from typing import List
 import logging
 
 
+""" fields from user_data.csv considered PII that must be hidden in logs """
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Obfuscates specified fields in the log message."""
@@ -31,3 +34,15 @@ class RedactingFormatter(logging.Formatter):
         """Format the log record, obfuscating specified fields."""
         msg = super().format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Create and configure a logger instance."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
