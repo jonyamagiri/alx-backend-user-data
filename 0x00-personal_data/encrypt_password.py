2b18b2effcfd0776d81320642154c7ff64afe6c2
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-28 18:29:45 +0300

    personal_data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(encoded_password, salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if the provided password matches the hashed password."""
+    encoded_password = password.encode('utf-8')
+    return bcrypt.checkpw(encoded_password, hashed_password)
