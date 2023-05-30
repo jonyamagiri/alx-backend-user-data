07b052120d1eee30e9f80d953c6769d31a79ac0d
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-30 20:50:17 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,23 +9,11 @@ class Auth:
     """Basic uthentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for the given path."""
-        if path is None or excluded_paths is None or not excluded_paths:
-            return True
-        if not path.endswith('/'):
-            path += '/'
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith('*'):
-                if path.startswith(excluded_path[:-1]):
-                    return False
-        if path in excluded_paths:
-            return False
-        return True
+        return False
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request."""
-        if request is None or "Authorization" not in request.headers:
-            return None
-        return request.headers.get("Authorization")
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current authenticated user."""
