97bf2cb325ecd017adfeef9cd1467442a6fd7744
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-30 14:59:23 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    """ endpoint for error handler 401 (request unauthorized)"""
+    abort(401)
