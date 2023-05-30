935fdeb5c6d2b9f687184608c5bcd1fc35bad624
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-30 21:51:30 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
     """Basic uthentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for the given path."""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request."""
