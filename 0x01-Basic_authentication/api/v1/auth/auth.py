4f9a8f1f25a649d8bc3846907322866d5aab535f
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-05-30 19:13:33 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,22 +8,46 @@ from typing import List, TypeVar
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for the given path.
+
         Args:
             path (str): The path being accessed.
             excluded_paths (List[str]): List of paths that are excluded from authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
         """
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request.
+
         Args:
             request (Request, optional): The Flask request object. Defaults to None.
+
+        Returns:
+            str: The authorization header value.
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get the current authenticated user.
+
         Args:
             request (Request, optional): The Flask request object. Defaults to None.
+
+        Returns:
+            TypeVar('User'): The current authenticated user.
         """
         return None
