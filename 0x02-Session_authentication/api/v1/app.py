f620ff842165a7bbf954120148487e6d9b04a866
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-02 08:19:37 +0300

    Session_authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,6 +54,7 @@ def before_request():
             request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
+        request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403)
 
