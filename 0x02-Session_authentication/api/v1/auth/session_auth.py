48955e9bbe003b5213f8cf978dd577cb1a315f39
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-02 14:29:27 +0300

    Session_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" module for session authentication """
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ class for session authentication """
+    pass
