2afe992a7f9f3cae79fb05fad296f5d4cf11e2b7
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-04 15:55:55 +0300

    Session_authentication_advanced

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,8 @@ auth = None
 auth_type = os.getenv("AUTH_TYPE")
 if auth_type == "session_auth":
     auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
 elif auth_type == "basic_auth":
     auth = BasicAuth()
 else:
