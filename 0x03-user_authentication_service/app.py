52034375be1f7df534f1a47fd176c52eeff73228
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-09 13:12:11 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,19 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """Update user password based on reset token."""
+    email = request.form.get('email')
+    token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(token, password)
+    except Exception:
+        abort(403)
+    response_data = {"email": email, "message": "Password updated"}
+    return jsonify(response_data), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
