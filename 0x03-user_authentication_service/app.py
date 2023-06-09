30072b18e1f41a5b38d133fc8992c4c5d20f6856
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-06-09 12:49:37 +0300

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,17 @@ def profile():
     return jsonify(response_data), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Retrieves a reset password token for a given email."""
+    try:
+        email = request.form.get('email')
+        token = AUTH.get_reset_password_token(email)
+        response = {"email": email, "reset_token": token}
+        return jsonify(response), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
